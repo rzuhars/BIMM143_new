c7b82ecaaea8028a83caaa9898a0ac3be2957c89
Renee Zuhars <reneezuhars@macbook-air-3.dynamic.ucsd.edu>
2025-06-03 11:38:33 -0700

    class 4-9, 16, 18, 19, gene project

---
+++ class04/class04.R
@@ -0,0 +1,10 @@
+#'---
+#'title: "Intro to R"
+#'author: "Renee Zuhars, A17329856"
+#'---
+
+# My First R Script
+x <- 1:50
+plot(x,sin(x))
+plot(x,sin(x),type = "l",col="violet",lwd=3,xlab="Silly x axis",ylab="Sensible y axis")
+
